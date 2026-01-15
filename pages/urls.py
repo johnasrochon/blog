865ae479eff808de14ba24972ba8f353acfced8d
john rochon <johnasrochon@gmail.com>
2026-01-14 22:30:39 -0600

    2assgin

--- pages/urls.py
+++ pages/urls.py
@@ -4,4 +4,5 @@ from .views import HomePageView
 urlpatterns = [
     path("", HomePageView.as_view(), name="home"),
     path("home/", HomePageView.as_view(), name="homeTwo"),
+    path("about/", AboutPageView.as_view(), name="about"),
 ]
